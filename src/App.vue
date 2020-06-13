fc78048596b0124310f57ab0af9437a85037c5f5
Kawaguchi Hiryu <hiryu@KawaguchinoMacBook-Pro.local>
2020-06-13 23:48:25 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+import firebase from 'firebase';
 
-export default {
-  name: 'App',
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: 'AIzaSyBTjRfyuO0SPuGj4hxD9ZjB_xUTkLaB6wk',
+  authDomain: 'vue-chat-69814.firebaseapp.com',
+  projectId: 'vue-chat-69814'
+});
+
+@Component({
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
 </script>
 
 <style>
