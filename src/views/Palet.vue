35cc47c2e599d57bbb313180d2ddc9ebc6860ed3
Kawaguchi Hiryu <hiryu.tubasa@gmail.com>
2020-08-22 15:52:47 +0900

    add page

---
+++ src/views/Palet.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <p>palet</p>
+      <input v-model="name" name="name" label="name">
+      <input v-model="gameId" name="gameId" label="gameId">
+      <button v-on:click="onclick()">submit</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Palet",
+  data() {
+    return {
+      name: "hoge",
+      gameId: "gameid"
+    };
+  },
+  methods: {
+    onclick() {
+      alert("a");
+    }
+  }
+});
+</script>
